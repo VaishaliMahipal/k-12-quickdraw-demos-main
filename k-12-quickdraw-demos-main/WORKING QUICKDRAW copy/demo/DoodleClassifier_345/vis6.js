70e8cacd7677ce4008e317c9c7d4309872fe31d3
VaishaliMahipal <74631552+VaishaliMahipal@users.noreply.github.com>
2022-03-11 02:08:29 -0500

    changes made

--- k-12-quickdraw-demos-main/WORKING QUICKDRAW copy/demo/DoodleClassifier_345/vis6.js
+++ k-12-quickdraw-demos-main/WORKING QUICKDRAW copy/demo/DoodleClassifier_345/vis6.js
@@ -1,7 +1,8 @@
-// Trained on 1 filters - 8 filters of 3 by 3 
+// Trained on 1 Convolution Layer with 8 filters of 3 by 3 
+
 const IMAGE_SIZE = 784;
 const CLASSES = ['cat','sheep','apple','door','cake','triangle']
-const k = 10;
+const k = 6;
 let model;
 let cnv;
 
@@ -29,6 +30,7 @@ function setup() {
   clearButton.mousePressed(() => {
     background(255);
     select('#outputLayer').html('');
+   
   });
 }
 
@@ -53,7 +55,11 @@ function guess() {
   const sortProb = rawProbWIndex.sort((a, b) => b.probability - a.probability);
   const topKClassWIndex = sortProb.slice(0, k);
   const topKRes = topKClassWIndex.map(i => `<br>${CLASSES[i.index]} (${(i.probability.toFixed(2) * 100)}%)`);
+  
   select('#outputLayer').html(` ${topKRes.toString()}`);
+  
+    
+  }
   
   const layer1 = model.getLayer('conv2d_2');
   console.log("layer1 =")
@@ -69,41 +75,61 @@ function guess() {
   nameL=[]
   nameL.push("conv2d_2")
   positionA=[]
-  positionA.push("filters1")
-  positionA.push("filters2")
-  positionA.push("filters3")
-  positionA.push("filters4")
-  positionA.push("filters5")
-  positionA.push("filters6")
-  positionA.push("filters7")
-  positionA.push("filters8")
+  positionA.push("Filter 1")
+  positionA.push("Filter 2")
+  positionA.push("Filter 3")
+  positionA.push("Filter 4")
+  positionA.push("Filter 5")
+  positionA.push("Filter 6")
+  positionA.push("Filter 7")
+  positionA.push("Filter 8")
   positionF=[]
-  positionF.push("featureMaps1")
-  positionF.push("featureMaps2")
-  positionF.push("featureMaps3")
-  positionF.push("featureMaps4")
-  positionF.push("featureMaps5")
-  positionF.push("featureMaps6")
-  positionF.push("featureMaps7")
-  positionF.push("featureMaps8")
+  positionF.push("Feature Map 1")
+  positionF.push("Feature Map 2")
+  positionF.push("Feature Map 3")
+  positionF.push("Feature Map 4")
+  positionF.push("Feature Map 5")
+  positionF.push("Feature Map 6")
+  positionF.push("Feature Map 7")
+  positionF.push("Feature Map 8")
+  positionT=[]
+  positionT.push("#text1")
+  positionT.push("#text2")
+  positionT.push("#text3")
+  positionT.push("#text4")
+  positionT.push("#text5")
+  positionT.push("#text6")
+  positionT.push("#text7")
+  positionT.push("#text8")
+  positionTF=[]
+  positionTF.push("#textF1")
+  positionTF.push("#textF2")
+  positionTF.push("#textF3")
+  positionTF.push("#textF4")
+  positionTF.push("#textF5")
+  positionTF.push("#textF6")
+  positionTF.push("#textF7")
+  positionTF.push("#textF8")
  
   
   const { filters, filterActivations } = getActivationTable(inputImage1,nameL[0]);
   console.log("filters in guess function")
   console.log(filters)
   console.log("activations in guess function")
-  //console.log(filterActivations)
+  console.log(filterActivations)
+  select('#heading2').html("Filters");
+  select('#heading3').html("Feature Maps");
   for (let i = 0; i < 8; i++) { 
-    console.log(filterActivations[0][i])
-    renderImage(positionA[i], filters[i], { width: 40, height: 40 })
-    renderImage(positionF[i], filterActivations[0][i], { width: 40, height: 40 })
+    
+    renderImage(positionA[i], filters[i], { width: 40, height: 40 },positionT[i])
+    renderImage(positionF[i], filterActivations[0][i], { width: 40, height: 40 },positionTF[i])
 
   }
   
   
 }
 
- async function renderImage(container, tensor, imageOpts) {
+ async function renderImage(container, tensor, imageOpts,textP) {
      console.log("tensor")
      console.log(tensor)
     const resized = tf.tidy(() =>
@@ -113,16 +139,18 @@ function guess() {
     const childs =document.getElementById(container); 
     
     
+    
     const canvas =  childs.querySelector('canvas') || document.createElement('canvas');
     canvas.width = imageOpts.width;
     canvas.height = imageOpts.height;
     canvas.style = `margin: 4px; :${imageOpts.width}px; height:${imageOpts.height}px`;
+    //canvas.style.borderRadius = '50%';
     childs.appendChild(canvas);
-    console.log("rsized")
-    console.log(resized)
+    
    
   
     await tf.browser.toPixels(resized, canvas);
+    select(textP).html(container);
     resized.dispose();
   }
 
@@ -137,23 +165,13 @@ function getActivationTable(image1,layerName) {
 
     let filters = tf.tidy(() => layer.kernel.val.transpose([3, 0, 1, 2]).unstack());
 
-    console.log("filters")
-    console.log(filters)
-    console.log(filters[0].shape[2])
-    
-    console.log("filters1")
-    console.log(filters)
-
     // Get the activations
     const activations = tf.tidy(() => {
       return getActivation(image1, model, layer).unstack();
     });
     const activationImageSize = activations[0].shape[0]; // e.g. 24
     const numFilters = activations[0].shape[2]; // e.g. 8
-    console.log("activationImage size=")
-    console.log(activationImageSize)
-    console.log("numFilters=")
-    console.log(numFilters)
+  
 
 
     const filterActivations = activations.map((activation, i) => {
